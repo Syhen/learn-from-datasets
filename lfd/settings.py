7d077b68aaca9e6d9c1ff9badbe1d386bee8ef3a
heyao <jackay-hy@qq.com>
2021-12-09 16:46:19 +0800

    add settings

---
+++ lfd/settings.py
@@ -0,0 +1,11 @@
+"""
+@created by: heyao
+@created at: 2021-12-08 14:30:10
+"""
+import pathlib
+import os
+
+_HOME = pathlib.Path(os.path.expanduser("~"))
+PATH_DATASETS = _HOME / "learn_from_datasets"
+
+NAME_DISASTER_TWEETS = "nlp-getting-started"
