2bd80841af5a14e9ac004e25b0278e7c288ce710
heyao <jackay-hy@qq.com>
2021-12-09 18:05:11 +0800

    push

--- lfd/disaster_tweets/bert_base.py
+++ lfd/disaster_tweets/bert_base.py
@@ -11,22 +11,27 @@ from torch.utils.data import Dataset
 from tqdm.auto import tqdm
 from transformers import AutoModel, AutoTokenizer
 
+from lfd.layers import Attention
+
 
 class Roberta(nn.Module):
-    def __init__(self):
+    def __init__(self, step_dim, model_path="/Users/heyao/Desktop/roberta-base"):
         super().__init__()
-        self.backbone = AutoModel.from_pretrained("/Users/heyao/Desktop/roberta-base")
+        self.backbone = AutoModel.from_pretrained(model_path)
         self.mean_pool = lambda x: torch.mean(x, dim=1)
         self.max_pool = lambda x: torch.max(x, dim=1)[0]
         self.flatten = nn.Flatten()
         self.dropout = nn.Dropout(0.1)
+        self.attention_layer = Attention(self.backbone.config.hidden_size, step_dim)
         # self.dropout = nn.Identity()
-        self.head = nn.Linear(self.backbone.config.hidden_size, 1)
+        self.head = nn.Linear(self.backbone.config.hidden_size * 2, 1)
 
     def forward(self, input_ids, attention_mask):
         outputs = self.backbone(input_ids, attention_mask)
         x = outputs.last_hidden_state
-        x = self.mean_pool(x)
+        mean_pool = self.mean_pool(x)
+        attention = self.attention_layer(x)
+        x = torch.cat([mean_pool, attention], dim=1)
         # print(x.shape)
         x = self.head(x)
         # print("output:", x.shape)
@@ -49,9 +54,9 @@ if __name__ == '__main__':
     y = train["target"].values
     X_train, X_val, y_train, y_val = train_test_split(train, y, test_size=0.2, random_state=42, stratify=y)
     # print(pd.DataFrame({"x": [len(i) for i in X]}).describe([.5, .75, .9, .99, .997]))
-
+    max_length = 85
     tokenizer = AutoTokenizer.from_pretrained("/Users/heyao/Desktop/roberta-base")
-    dataset = DisasterTweetsBertDataset(X_train["text"].to_list(), y_train, tokenizer)
+    dataset = DisasterTweetsBertDataset(X_train["text"].to_list(), y_train, tokenizer, max_length=max_length)
     # lengths = []
     # for x, _ ,y in dataset:
     #     lengths.append(len(x))
@@ -59,9 +64,9 @@ if __name__ == '__main__':
 
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
 
-    val_dataset = DisasterTweetsBertDataset(X_val["text"].to_list(), y_val, tokenizer)
+    val_dataset = DisasterTweetsBertDataset(X_val["text"].to_list(), y_val, tokenizer, max_length=max_length)
     val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=64, shuffle=True)
-    model = Roberta()
+    model = Roberta(step_dim=max_length)
     criterion = nn.BCEWithLogitsLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=3e-5, weight_decay=1e-4)
     best_score = -float("inf")
